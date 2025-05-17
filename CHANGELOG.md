e6317db7a306a8c8aeb062f8f7808c2790beb8e3
Victor Gonzalez <victorglez1989@gmail.com>
2025-05-17 19:18:01 +0000

    Merged PR 118: Release new template
    ## [v19.0.1] (2025-05-17)
    
    - Update Ci/Cd pipeline

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project from 0.1.x forward will be documented in this file.
 
+## [v19.0.1] (2025-05-17)
+
+- Update Ci/Cd pipeline
+
 ## [v19.0.0] (2025-04-12)
 
 - Include new Template for Angular v19
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-istreamer'),
+      dir: require('path').join(__dirname, '../../coverage/template-nodejs-angular-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
